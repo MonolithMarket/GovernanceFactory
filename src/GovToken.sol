68d2af9f99578866d16e9b9232bcb9afa9bf4bc1
08xmt <markus.08xmt@gmail.com>
2026-08-26 15:59:29 +0200

    Add ERC20Votes functionality

--- src/GovToken.sol
+++ src/GovToken.sol
@@ -5,10 +5,14 @@ import {ERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/
 import {
     ERC20PermitUpgradeable
 } from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
+import {
+    ERC20VotesUpgradeable
+} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20VotesUpgradeable.sol";
+import {NoncesUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/NoncesUpgradeable.sol";
 
 uint256 constant GOV_TOKEN_SUPPLY = 10_000_000 * 1e18;
 
-contract GovToken is ERC20Upgradeable, ERC20PermitUpgradeable {
+contract GovToken is ERC20Upgradeable, ERC20PermitUpgradeable, ERC20VotesUpgradeable {
     constructor() {
         _disableInitializers();
     }
@@ -18,7 +22,19 @@ contract GovToken is ERC20Upgradeable, ERC20PermitUpgradeable {
 
         __ERC20_init(name_, symbol_);
         __ERC20Permit_init(name_);
+        __ERC20Votes_init();
         _mint(initialHolder, GOV_TOKEN_SUPPLY);
+    }
+
+    function _update(address from, address to, uint256 value)
+        internal
+        override(ERC20Upgradeable, ERC20VotesUpgradeable)
+    {
+        super._update(from, to, value);
+    }
+
+    function nonces(address owner) public view override(ERC20PermitUpgradeable, NoncesUpgradeable) returns (uint256) {
+        return super.nonces(owner);
     }
 
     error ZeroAddress();
